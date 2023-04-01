26f75835a74a1f3a3d6c945cefc6b56e42a21a9b
serega467 <sergeyurievich90@gmail.com>
2023-04-01 22:39:13 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -22,3 +22,15 @@ function open(evt) {
    tabTarget.classList.add('tabs__btn-item--active');
     document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
+
+// процесс нажатия на нашу кнопку (бургер меню) будет храниться в константе menuBtn
+const menuBtn = document.querySelector('.menu-btn');
+
+// процесс нажатия на наше меню будет храниться в константе menu
+const menu = document.querySelector('.menu');
+
+// благодаря обработчику событий - addEventListener, при клике на нашу кнопку (бургер меню), будет происходить следующее
+menuBtn.addEventListener('click', () => {
+    //к классу .menu будет добавляться класс .menu--active
+    menu.classList.toggle('menu--active');
+});
